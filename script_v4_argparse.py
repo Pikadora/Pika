70a1d8f502e8c159375c32791663f05ca65121d6
Pikadora <49182643+Pikadora@users.noreply.github.com>
2019-04-05 19:01:48 +0500

    Update script_v4_argparse.py
    05.05.2019

--- script_v4_argparse.py
+++ script_v4_argparse.py
@@ -64,19 +64,18 @@ def kolvo_oper():
     else:
         sys.exit(1)
 
-parser = argparse.ArgumentParser(description='Parsing some files.')
-parser.add_argument('-s','--services',help = 'Check services',action = 'store_true')
-parser.add_argument('-t','--trunks',help = 'Check status of sip trunks',action = 'store_true')
-parser.add_argument('-u','--users',help = 'Check number of authorized users ',action = 'store_true')
+parser = argparse.ArgumentParser(description='Three check options.')
+parser.add_argument('-m','--mode',help = 'Check Modes: services,sip_trunks or operators')
 args = parser.parse_args()
 
-if args.services:
+if args.mode == "services":
         service_stat()
-elif args.trunks:
+elif args.mode == "sip_trunks":
         sip_trunk()
-elif args.users:
+elif args.mode == "operators":
         kolvo_oper()
 else:
         print('Please see th HELP: "python test.py -h" or "python test.py --help" and try again')
+
 
 
