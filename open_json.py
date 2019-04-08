0bc9f076f84eea640957489a5cece9439510a06d
Pikadora <49182643+Pikadora@users.noreply.github.com>
2019-04-08 18:17:36 +0500

    Update open_json.py
    argparse + open json + script_all

--- open_json.py
+++ open_json.py
@@ -1,25 +1,27 @@
 import json
 import argparse
-import script
+import script_all
 
 parser = argparse.ArgumentParser(description='Three check options.')
 parser.add_argument('-m','--mode',help = 'Check Modes: services,sip_trunks or operators')
-args = parser.parse_args()
+args_con = parser.parse_args()
 
-with open('/tmp/pika/checks.json','r')as lafa:
-    data = json.load(lafa)
-    for i in range(len(data['checks'])):
-        if args.mode in data['checks'][i]['name'] == "services"  and data['checks'][i]['success'] == 'true':
-            print(data['checks'][i])
-            print("__________________")
-            script.service_stat()
-        elif args.mode in data['checks'][i]['name'] == "sip_trunks" and data['checks'][i]['success'] == 'true':
-            print(data['checks'][i])
-            print("__________________")
-            script.sip_trunk()
-        elif args.mode in data['checks'][i]['name'] == "operators" and data['checks'][i]['success'] == 'true':
-            print(data['checks'][i])
-            print("__________________")
-            script.kolvo_oper()
-        else:
-            print('Please see th HELP: "python test.py -h" or "python test.py --help" and try again')
+
+if args_con.mode is None :
+    print('____________________')
+    print('Please see the HELP: "python test.py -h" or "python test.py --help" and try again')
+    print('____________________')
+
+else:
+    with open('/tmp/pika/checks.json','r')as lafa:
+        data = json.load(lafa)
+        for i in range(len(data['checks'])):
+            wtfk = data['checks'][i]
+            if args_con.mode == wtfk['name']:
+                if data['checks'][i]['success'] == 'true':
+                    cod = data['checks'][i]['command']
+                    exec(cod)
+                else:
+                    print('Please see the config_file (check success true/false)')
+            else:
+                continue
