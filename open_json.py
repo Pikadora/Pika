d98606f30f2652dee4d62f0fd0ba5134dce343b6
Pikadora <49182643+Pikadora@users.noreply.github.com>
2019-04-08 16:13:54 +0500

    Update open_json.py
    use script.py(all functions) and check.json(checks = true/false)

--- open_json.py
+++ open_json.py
@@ -1,19 +1,23 @@
 import json
+import argparse
 import script
 
+parser = argparse.ArgumentParser(description='Three check options.')
+parser.add_argument('-m','--mode',help = 'Check Modes: services,sip_trunks or operators')
+args = parser.parse_args()
 
 with open('/tmp/pika/checks.json','r')as lafa:
     data = json.load(lafa)
     for i in range(len(data['checks'])):
-        if data['checks'][i]['name'] == 'services' and data['checks'][i]['success'] == 'true':
+        if args.mode in data['checks'][i]['name'] == "services"  and data['checks'][i]['success'] == 'true':
             print(data['checks'][i])
             print("__________________")
             script.service_stat()
-        elif data['checks'][i]['name'] == 'sip_trunks' and data['checks'][i]['success'] == 'true':
+        elif args.mode in data['checks'][i]['name'] == "sip_trunks" and data['checks'][i]['success'] == 'true':
             print(data['checks'][i])
             print("__________________")
             script.sip_trunk()
-        elif data['checks'][i]['name'] == 'operators' and data['checks'][i]['success'] == 'true':
+        elif args.mode in data['checks'][i]['name'] == "operators" and data['checks'][i]['success'] == 'true':
             print(data['checks'][i])
             print("__________________")
             script.kolvo_oper()
