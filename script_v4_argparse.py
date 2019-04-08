9400add1cb99cd75a393a176e81419f707fbd457
Pikadora <49182643+Pikadora@users.noreply.github.com>
2019-04-08 14:20:57 +0500

    Update script_v4_argparse.py
    only functions

--- script_v4_argparse.py
+++ script_v4_argparse.py
@@ -1,4 +1,3 @@
-import argparse
 import subprocess
 import sys
 import re
@@ -63,19 +62,3 @@ def kolvo_oper():
         sys.exit(0)
     else:
         sys.exit(1)
-
-parser = argparse.ArgumentParser(description='Three check options.')
-parser.add_argument('-m','--mode',help = 'Check Modes: services,sip_trunks or operators')
-args = parser.parse_args()
-
-if args.mode == "services":
-        service_stat()
-elif args.mode == "sip_trunks":
-        sip_trunk()
-elif args.mode == "operators":
-        kolvo_oper()
-else:
-        print('Please see th HELP: "python test.py -h" or "python test.py --help" and try again')
-
-
-
